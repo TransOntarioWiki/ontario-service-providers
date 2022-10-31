50e64fe553e44aa5aadb11da091396a7d1302aa4
Jocelyn Stericker <jocelyn@nettek.ca>
2022-10-31 08:48:10 -0400

    Unselect pills

--- frontend/src/PillButtonInput.jsx
+++ frontend/src/PillButtonInput.jsx
@@ -7,15 +7,15 @@ const PillButtonInput = ({ name, value, label }) => {
   const onClick = () => {
     if (ref.current) {
       ref.current.value = value;
-      input.onChange(value);
+      input.onChange(input.value === value ? "" : value);
     }
   };
 
   const selectedClasses =
-    "text-white bg-blue-500 hover:bg-blue-700 hover:drop-shadow-md";
+    "text-white bg-blue-500 hover:bg-blue-700 hover:drop-shadow-md border-blue-500 hover:border-blue-700";
   const unselectedClasses =
-    "text-blue-500 hover:text-blue-700 border border-pink-500 hover:border-pink-700 ";
-  const baseClasses = "py-1 px-3 rounded-full text-sm duration-100 ";
+    "text-blue-500 hover:text-blue-700 border-pink-500 hover:border-pink-700 ";
+  const baseClasses = "border py-1 px-3 rounded-full text-sm duration-100 ";
 
   const className =
     input.value === value
