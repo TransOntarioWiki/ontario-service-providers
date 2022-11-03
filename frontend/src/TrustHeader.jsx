4d8b38bdc76881737ed8fea0b807d3eb5ec36800
Jocelyn Stericker <jocelyn@nettek.ca>
2022-11-03 19:48:00 -0400

    Remove flag

--- frontend/src/TrustHeader.jsx
+++ frontend/src/TrustHeader.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function TrustHeader() {
   return (
-    <div className="w-full text-center bg-sky-100 p-1 border-black border-b">
-      🏳️‍⚧️ A volunteer-run service for trans Ontarians, by trans Ontarians.
+    <div className="w-full text-center bg-sky-100 p-1">
+      A volunteer-run service for trans Ontarians, by trans Ontarians.
     </div>
   );
 }
