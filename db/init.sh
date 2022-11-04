e32318c6fe200700864cda713dcd9ebd3cc54c5e
Jocelyn Stericker <jocelyn@nettek.ca>
2022-11-03 21:16:44 -0400

    update init.sh

--- db/init.sh
+++ db/init.sh
@@ -8,5 +8,6 @@ psql -p 5490 -U transontario < ./02_rho.sql
 psql -p 5490 -U transontario < ./03_api.sql
 psql -p 5490 -U transontario < ./04_auth.sql
 psql -p 5490 -U transontario < ./05.sql
+psql -p 5490 -U transontario < ./06.sql
 pg_ctl stop -D ./db
 
