921e4440ffe293e6e7bdaf3842e03eb3fe4ca80b
Jocelyn Stericker <jocelyn@nettek.ca>
2022-11-06 11:05:52 -0500

    remove final form from top search

--- frontend/src/SearchForm.jsx
+++ frontend/src/SearchForm.jsx
@@ -1,29 +1,9 @@
-import { useEffect } from "react";
-import { Form, Field } from "react-final-form";
 import { ReactSearchAutocomplete } from "react-search-autocomplete";
 import { useSearchParams } from "react-router-dom";
 
 import regions from "./regions";
 import { useServices } from "./api";
 import PillButtonInput from "./PillButtonInput";
-
-const autoCompleteWrapper = ({ input, items }) => (
-  <ReactSearchAutocomplete
-    items={items}
-    onSelect={(item) => input.onChange(`cs.{${item.name}}`)}
-    onSearch={(search) => input.onChange(search)}
-  />
-);
-
-const Listener = ({ values }) => {
-  const [, setSearchParams] = useSearchParams();
-
-  useEffect(() => {
-    setSearchParams(values);
-  }, [setSearchParams, values]);
-
-  return null;
-};
 
 function paramsToObject(entries) {
   const result = {};
@@ -37,39 +17,54 @@ function paramsToObject(entries) {
 const SearchForm = () => {
   const servicesData = useServices();
   const services = servicesData.data;
-  const [searchParams] = useSearchParams();
+  const [searchParams, setSearchParams] = useSearchParams();
 
   return (
-    <Form onSubmit={() => {}} initialValues={paramsToObject(searchParams)}>
-      {({ handleSubmit, values }) => (
-        <form onSubmit={handleSubmit}>
-          <Listener values={values} />
-          <div className="flex flex-wrap justify-center gap-2 mb-4">
-            {Object.entries(regions).map(([api, ux]) => (
-              <PillButtonInput
-                key={api}
-                name="region"
-                value={api === "null" ? "is.null" : `eq.${api}`}
-                label={ux}
-              />
-            ))}
-          </div>
-          <div className="flex flex-col items-center">
-            <label htmlFor="services" className="w-fit mr-4">
-              Filter by Service
-            </label>
-            <div className="w-full">
-              <Field
-                type="text"
-                component={autoCompleteWrapper}
-                name="services"
-                items={services}
-              />
-            </div>
-          </div>
-        </form>
-      )}
-    </Form>
+    <form>
+      <div
+        className="flex flex-wrap justify-center gap-2 mb-4"
+        onChange={(ev) => {
+          setSearchParams({
+            ...paramsToObject(searchParams),
+            region: ev.target.value,
+          });
+        }}
+      >
+        {Object.entries(regions).map(([api, ux]) => (
+          <PillButtonInput
+            key={api}
+            name="region"
+            value={api === "null" ? "is.null" : `eq.${api}`}
+            label={ux}
+            currentValue={searchParams.get("region")}
+          />
+        ))}
+      </div>
+      <div className="flex flex-col items-center">
+        <label htmlFor="services" className="w-fit mr-4">
+          Filter by Service
+        </label>
+        <div className="w-full">
+          {services ? (
+            <ReactSearchAutocomplete
+              items={services}
+              onSelect={(item) => {
+                setSearchParams({
+                  ...paramsToObject(searchParams),
+                  services: `cs.{${item.name}}`,
+                });
+              }}
+              onClear={() => {
+                const obj = paramsToObject(searchParams);
+                delete obj.services;
+                setSearchParams(obj);
+              }}
+              autoFocus={false}
+            />
+          ) : null}
+        </div>
+      </div>
+    </form>
   );
 };
 
