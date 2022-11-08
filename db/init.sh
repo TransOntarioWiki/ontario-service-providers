6183b819df2e6da329d0bfd7114fbfc693c226d3
Jocelyn Stericker <jocelyn@nettek.ca>
2022-11-07 20:03:25 -0500

    Add migration

--- db/init.sh
+++ db/init.sh
@@ -5,5 +5,6 @@ pg_ctl -o "-p 5490" start -D ./db
 createdb -p 5490 -U transontario transontario
 psql -p 5490 -U transontario < ./01_init.sql
 psql -p 5490 -U transontario < ./02_review_timestamp.sql
+psql -p 5490 -U transontario < ./03_create_provider_fixes.sql
 pg_ctl stop -D ./db
 
